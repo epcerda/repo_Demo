9191d373527eaf93ce237c20ed6eca868cf602d2
ckilmurray <ckilmurray@gmail.com>
2023-03-16 08:43:04 -0300

    comments added.

--- ejerDemo.hs
+++ ejerDemo.hs
@@ -1,6 +1,7 @@
 --Factorial
 --Ejer para demostración de Git
+-- año 2023
 
 factorial :: Integer -> Integer
-factorial 1 = 1
-factorial n = n * factorial (n - 1)
+factorial 1 = 1 -- caso base
+factorial n = n * factorial (n - 1) -- llamada recursiva
